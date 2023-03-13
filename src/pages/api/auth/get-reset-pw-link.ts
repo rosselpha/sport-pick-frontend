961b4f10dc15347c8a18bbebad83e668347568cb
ross elpharin <elpharin@gmail.com>
2023-03-13 11:41:48 -0400

    promise

--- src/pages/api/auth/get-reset-pw-link.ts
+++ src/pages/api/auth/get-reset-pw-link.ts
@@ -4,6 +4,20 @@ import User from '@/lib/models/user';
 import { emailLayout, resestEmail} from "@/lib/services/emaillayout";
 import { transporter } from "@/lib/utils/nodemailer"
 
+
+function getToken(res: any) {
+return new Promise((resolve, reject) => {
+    crypto.randomBytes(32, (err, buffer) => {
+    if (err) {
+        res.json({ message: "err with link" });
+        reject(err);
+    } else {
+        resolve(buffer.toString("hex"));
+    }
+    });
+    });
+}
+
 export default async function handleReset(req:any, res:any){
 
     const origin = req.headers.origin
@@ -19,28 +33,33 @@ export default async function handleReset(req:any, res:any){
         const existingUser = await User.findOne({email: userEmail}).exec()
         if(!existingUser){
             return res.status(422).json({message: "email doesn't exsist signup now"})
-        }
-        crypto.randomBytes(32, (err, buffer)=> {
-            if(err){
-                console.log(err) 
-                return res.json({message: "err with link"})
-            }
-            const token = buffer.toString('hex')
-            existingUser.resetToken = token
-            existingUser.resetTokenExpiration = Date.now()+ 3600000
-            existingUser.save()
+        }       
+        const token = await getToken(res)
+            
+        existingUser.resetToken = token
+        existingUser.resetTokenExpiration = Date.now()+ 3600000
+        existingUser.save()
 
-            const email:string = emailLayout(resestEmail(token, origin))
-            transporter.sendMail({
-                to:userEmail,
-                from: 'ross.team@blockplay.me',
-                subject: 'Reset Password Link',
-                html: email
-            }, (err, info) =>{
-                if(err) console.log(err)
-                console.log(`res= ${info.response}`)
-            })
+
+        const email:string = emailLayout(resestEmail(token, origin))
+
+        await new Promise((resolve, reject) => {
+        transporter.sendMail({
+            to:userEmail,
+            from: 'ross.team@blockplay.me',
+            subject: 'Reset Password Link',
+            html: email
+        }, (err, info) =>{
+            if(err) {
+                console.log(err)
+                reject(err)
+            }
+            console.log(`res= ${info.response}`)
+            resolve(info)
+
         })
+        
+    })
 
         return res.json({message: "password reset link have been sent"})
 
